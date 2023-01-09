009bf3baec9d36ff64e664cd15bf750b72d9d320
Ruslan Lyubimov <ruslan_lyubimov@epam.com>
2023-01-09 15:49:50 +0500

    Added final solution for hw1

---
+++ hw1/task2.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+
+const CONSTANTS = require("./constants");
+
+const writeOutput = fs.createWriteStream("output.txt");
+const source = fs
+  .createReadStream(CONSTANTS.PATH_TASK_2)
+  .pipe(csv())
+  .on("data", (data) => {
+    writeOutput.write(data);
+  })
+  .on("error", (err) => {
+    console.log("Stop! We have an error", err);
+  })
+  .on("end", () => {
+    console.log("Finished");
+  });
